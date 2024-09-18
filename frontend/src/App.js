bb35ddc7beb0e765cef3a074d47c087cbbc0bcb4
renanvilabela <vilabelarenan@gmail.com>
2024-09-18 17:40:16 -0300

    atualização no app.js e começo de refatoração no css da página

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
 import { useState } from 'react';
+import styles from '../src/App.css';
 
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState('');
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+    return passwordRegex.test(password);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+  let validationErrors = {}
+
+  //validação de email
+  if(!email) {
+    validationErrors.email = 'E-mail não pode estar vazio.';
+  }else if (validateEmail(email)) {
+    validationErrors.email = 'Formato de email inválido.';
+  }
+
+  //validação de senha
+  if (!password) {
+    validationErrors.password = 'Senha não pode estar vazia.';
+  }else if (validatePassword(password)) {
+    validationErrors.password = 'A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial.'
+  }
+
+  if (Object.keys(validationErrors).length > 0) {
+    setErrors(validationErrors);
+    setMessage('');
+  } else {
+    setErrors({})
+  }
     try {
       const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
